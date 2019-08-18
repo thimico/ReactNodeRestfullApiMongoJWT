03fbd8f2f4d298cf6a8ad553c806c2a2add1ddae
Thiago Oliveira <thiago.oliveira@wexinc.com>
2019-08-17 23:08:45 -0300

    Profile Item

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,77 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Spinner from '../common/Spinner';
+import ProfileItem from './ProfileItem';
+import { getProfiles } from '../../actions/profileActions';
 
 class Profiles extends Component {
-    render() {
-        return (
-            <div className="profiles">
-                <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                    <h1 className="display-4 text-center">Developer Profiles</h1>
-                    <p className="lead text-center">Browse and connect with developers</p>
+  componentDidMount() {
+    this.props.getProfiles();
+  }
 
-                    <div className="card card-body bg-light mb-3">
-                        <div className="row">
-                        <div className="col-2">
-                            <img className="rounded-circle" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200" alt="" />
-                        </div>
-                        <div className="col-lg-6 col-md-4 col-8">
-                            <h3>John Doe</h3>
-                            <p>Developer at Microsoft</p>
-                            <p>Seattle, WA</p>
-                            <a href="profile.html" className="btn btn-info">View Profile</a>
-                        </div>
-                        <div className="col-md-4 d-none d-lg-block">
-                            <h4>Skill Set</h4>
-                            <ul className="list-group">
-                            <li className="list-group-item">
-                                <i className="fa fa-check pr-1"></i>HTML</li>
-                            <li className="list-group-item">
-                                <i className="fa fa-check pr-1"></i>CSS</li>
-                            <li className="list-group-item">
-                                <i className="fa fa-check pr-1"></i>JavaScript</li>
-                            <li className="list-group-item">
-                                <i className="fa fa-check pr-1"></i>Python</li>
-                            <li className="list-group-item">
-                                <i className="fa fa-check pr-1"></i>C#</li>
-                            </ul>
-                        </div>
-                        </div>
-                    </div>
+  render() {
+    const { profiles, loading } = this.props.profile;
+    let profileItems;
 
-                    <div className="card card-body bg-light mb-3">
-                        <div className="row">
-                        <div className="col-2">
-                            <img className="rounded-circle" src="https://www.gravatar.com/avatar/anything?s=200&d=mm" alt="" />
-                        </div>
-                        <div className="col-lg-6 col-md-4 col-8">
-                            <h3>John Doe</h3>
-                            <p>Developer at Microsoft</p>
-                            <p>Seattle, WA</p>
-                            <a href="profile.html" className="btn btn-info">View Profile</a>
-                        </div>
-                        <div className="col-md-4 d-none d-lg-block">
-                            <h4>Skill Set</h4>
-                            <ul className="list-group">
-                            <li className="list-group-item">
-                                <i className="fa fa-check pr-1"></i>HTML</li>
-                            <li className="list-group-item">
-                                <i className="fa fa-check pr-1"></i>CSS</li>
-                            <li className="list-group-item">
-                                <i className="fa fa-check pr-1"></i>PHP</li>
-                            <li className="list-group-item">
-                                <i className="fa fa-check pr-1"></i>MySQL</li>
-                            </ul>
-                        </div>
-                        </div>
-                    </div>
-
-                    </div>
-                </div>
-                </div>
-            </div>
-        )
+    if (profiles === null || loading) {
+      profileItems = <Spinner />;
+    } else {
+      if (profiles.length > 0) {
+        profileItems = profiles.map(profile => (
+          <ProfileItem key={profile._id} profile={profile} />
+        ));
+      } else {
+        profileItems = <h4>No profiles found...</h4>;
+      }
     }
+
+    return (
+      <div className="profiles">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Developer Profiles</h1>
+              <p className="lead text-center">
+                Browse and connect with developers
+              </p>
+              {profileItems}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Profiles
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, { getProfiles })(Profiles);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import errReducer from './errorReducer';
+import profileReducer from './profileReducer';
 
 export default combineReducers({
     auth: authReducer,
-    errors: errReducer
+    errors: errReducer,
+    profile: profileReducer
 })
 
